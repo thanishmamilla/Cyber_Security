e0571fc9c25e6ba21cedc6fb20b8eafb1ce2d0cb
thanishmamilla <129726162+thanishmamilla@users.noreply.github.com>
2024-09-08 11:59:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ cloudinary.v2.config({
     secure:process.env.CLOUDINARY_SECURE,
 })
 
-// export const razorpay =new Razorpay({
-//     key_id: process.env.RAZORPAY_KEY_ID,
-//     key_secret: process.env.RAZORPAY_SECRET,
-// })
+export const razorpay =new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_SECRET,
+})
 
 
 app.listen(PORT,async ()=>{
